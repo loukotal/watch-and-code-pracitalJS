888c96ea4de77945ab6668fd3d34aa748d1578a2
Lukáš Loukota <lukasloukota@MacBook-Pro.local>
2017-05-08 19:16:13 +0200

    Modified to Version 4 - Booleans

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
+//V4 requirements:
+//todoList.addTodo should add objects
+//todoList.changeTodo should change the todoText property
+//todoList.toggleCompleted should change the completed property
+
 var todoList = {
     todos: [],
     displayTodos: function() {
         console.log(this.todos);
     },
-    addTodo: function(toAdd) {
-        this.todos.push(toAdd);
+    addTodo: function(todoText) {
+        this.todos.push(
+            {
+                todoText: todoText,
+                completed: false
+        });
         this.displayTodos();
     },
-    changeTodo: function(position, newValue) {
-        this.todos[position] = newValue;
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     removeTodo: function(position) {
         this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        //more logical
+        todo.completed = !todo.completed;
+        // my clunky implementation
+        // if(this.todos[position].completed) {
+        //     this.todos[position].completed = false;
+        // } else {
+        //     this.todos[position].completed = true;
+        // }
         this.displayTodos();
     }
     
@@ -23,5 +44,8 @@ todoList.addTodo("item 1")
 todoList.addTodo("item 2")
 todoList.changeTodo(0, "changed")
 todoList.removeTodo(1);
+todoList.toggleCompleted(0);
+todoList.toggleCompleted(0);
+todoList.toggleCompleted(0);
 
 
